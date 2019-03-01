dabbbc2c65f44d2f1fe8aa0b723ccaed032152db
biggodw <biggodw@gmail.com>
2019-03-01 22:53:55 +0800

    2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-window.onload=function(){
+
+$(function(){
     var mySwiper = new Swiper ('.swiper-container', {
         direction: 'horizontal', // 垂直切换选项
         loop: true, // 循环模式选项
@@ -8,4 +9,52 @@ window.onload=function(){
         el: '.swiper-pagination',
         },
     })  
-}
+})
+
+$('.header-nav .nav').tap(
+    function(e){
+        if(e.target.nodeName == "LI"){
+            $('.header-nav .nav li').removeClass('active')
+            e.target.classList.add('active')
+
+        }
+    }
+)
+
+$('.header-nav .more').tap(
+    function(){
+        this.classList.toggle('rotate')
+        $('.header-nav .nav').toggleClass('disnone')
+        if($('.boxMore').hasClass('block')){
+            $('.moreNav').addClass('fadeOutUp')
+            $('.moreNav').one('animationend',function(){
+                    $('.moreNav').removeClass('fadeOutUp')
+                    $('.boxMore').toggleClass('block')
+                }
+            )
+        }else{
+            $('.boxMore').toggleClass('block')
+            $('.moreNav').addClass('fadeInDown')
+            $('.moreNav').one('animationend',function(){
+            $('.moreNav').removeClass('fadeInDown')
+        })
+        }
+        
+    }
+)
+$('.footer li').tap(
+    function(e){
+        
+            $('.footer li').removeClass('active')
+            this.classList.add('active')
+            
+    }
+)
+$('.moreNav .list div').tap(
+    function(e){
+            console.log(e.target)
+            $('.moreNav .list div').removeClass('active')
+            this.classList.add('active')
+            
+    }
+)
